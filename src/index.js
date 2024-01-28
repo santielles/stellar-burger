0c27db1ea27ea3be79e7f7cf2079b4ce9acf3659
Elena Botkina <elena.botkina51@gmail.com>
2024-01-27 19:45:17 -0500

    Change BrowserRouter to HashRouter in order to fix GitHub Pages 404

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom';
+// GitHub Pages не умеет работать с роутером, поэтому используем HashRouter вместо BrowserRouter, хоть это и не рекомендуется
+import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
     <StrictMode>
-      <BrowserRouter>
+      {/* GitHub Pages не умеет работать с роутером, поэтому используем HashRouter вместо BrowserRouter, хоть это и не рекомендуется */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>
   </Provider>
 );
